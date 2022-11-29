9d5c4b52597ab41840a1167336760d9f33c8db44
Woody <kazettique@gmail.com>
2022-11-29 20:32:27 +0800

    add meta example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import LayoutA from '@/components/LayoutA';
 import { NextPageWithLayout } from '@/types';
+import Head from 'next/head';
 import Link from 'next/link';
 import { ReactElement } from 'react';
 
 const Home: NextPageWithLayout = () => {
   return (
     <div>
+      <Head>
+        <title>Meta Tag Example</title>
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <meta name="google" content="notranslate" key="notranslate" />
+        <link
+          rel="canonical"
+          href="https://example.com/blog/original-post"
+          key="canonical"
+        />
+      </Head>
       <div>this is home page</div>
       <div>
         <Link href="/about" className="p-2 m-2 rounded bg-neutral-300 block">
